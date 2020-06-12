0f70a162240e46cb33a5d11c95495b2a97781132
Nathanael Garcia <nrzgarcia@gmail.com>
2020-06-12 15:53:53 -0400

    info component

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import info from "./info";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Sherwins List</Text>
-    </View>
-  );
+  return <info />;
 }
 
 const styles = StyleSheet.create({
---
+++ info.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+class info extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Nate Garcia</Text>
+        <Text> Hollywood, FL</Text>
+        <Text> Hi, I am a volunteer</Text>
+      </View>
+    );
+  }
+}
+
+export default info;
